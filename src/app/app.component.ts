b6d63bf0f0240092f1cc6ede951f3e54a1349162
Munalula-Sikazwe <munalulasikazwe67@gmail.com>
2021-06-06 11:49:55 +0200

    unsubcribed from subject at component destruction to avoid memory leaks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {UserService} from "./user.service";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit,OnDestroy {
+  subscription! : Subscription;
   constructor(private userService:UserService) {}
 activated : boolean = false;
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(activate=>{
+    this.subscription = this.userService.activatedEmitter.subscribe(activate=>{
       this.activated = activate;
     })
+
+  }
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
 
